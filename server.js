f2c9c3a450939e63a8742eb3167453468d11bf29
Dendup1234 <dendup091@gmail.com>
2026-03-31 12:20:41 +0600

    Connected with db

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from "express";
-const app = express();
-const PORT = 3000;
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
-// Define a route for GET requests to the root URL
+// defining the imports
+const app = express();
+dotenv.config();
+
+// env variables
+const PORT = process.env.PORT;
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World from Express!");
 });
+
+await connectDB();
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
